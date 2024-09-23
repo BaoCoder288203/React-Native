63d6293010922190c4b32c89127a0f579a52f982
BaoCoder288203 <baon6777@gmail.com>
2024-09-23 10:44:40 +0700

    Delete all file

--- bai9/script.js
+++
@@ -1,9 +0,0 @@
-
-var arr = [17, 21, 23]
-var arr1 = [12, 5, -5, 0, 4]
-
-function printForecast(arr) {
-    arr.map((item,index)=>{
-        console.log(item + "*C" + "in" + index + "days");
-    })
-}
